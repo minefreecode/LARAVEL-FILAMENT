2ceeac449160bae285eb963f7565a8f6d7463907
aleksey <aleksey.work.job@gmail.com>
2025-03-14 15:24:06 +0300

    [doc] Документирование Начинаем комментирование

--- app/Actions/FetchPluginDataFromAnystack.php
+++ app/Actions/FetchPluginDataFromAnystack.php
@@ -8,23 +8,36 @@ use Throwable;
 
 use function Filament\Support\format_money;
 
+/**
+ * Вызывется из команды получения данных из Anystack
+ * Это сервис лицензирования
+ */
 class FetchPluginDataFromAnystack
 {
     public function __invoke(): void
     {
+        //Делается объект запроса с токеном
         $anystack = Http::withToken(config('services.anystack.token'));
 
         try {
+            //Получает данные каналов с anystack в виде json
             $advertismentChannels = $anystack
                 ->get('https://api.anystack.sh/v1/affiliate-beta')
                 ->json()['data'] ?? [];
 
+            /**
+             * collect создает коллекцию из массива
+             * keyBy - добавляет записям ключи 'id'
+             * ['da7855a9-36a1-44a4-87b9-8e5852ae08d2']  - выбирает элемент коллекции по данному ключу
+             */
             $advertisementChannel = collect($advertismentChannels)->keyBy('id')['da7855a9-36a1-44a4-87b9-8e5852ae08d2'] ?? null;
 
+            //Если каналы не получены ничего не обрабатываем
             if (! $advertisementChannel) {
                 return;
             }
 
+            // Для полученного канала получаем продукты по ключу `products`
             $advertisedProducts = collect($advertisementChannel['products'] ?? [])->keyBy('id');
 
             Plugin::query()
